36d04055a3c002484585fa6b59ca95c35b815237
MrEgor123 <144677952+MrEgor123@users.noreply.github.com>
2023-11-21 23:21:36 +0300

    Add files via upload

---
+++ Paskal Задание №1.pas
@@ -0,0 +1,30 @@
+﻿Program DKR_1;
+
+const
+  Sz = 5;
+
+Var
+  a: array[1..Sz] of Integer;
+  i, max, min: integer;
+
+begin
+  writeln('Введите данные массива: ');
+
+  for i := 1 to Sz do
+    readln(a[i]);
+
+  max := a[1];
+  min := a[1];
+
+  for i := 2 to Sz do
+  begin
+    if a[i] > max then
+      max := a[i];
+
+    if a[i] < min then
+      min := a[i];
+  end;
+
+  writeln('Максимальное число: ', max);
+  writeln('Минимальное число: ', min);
+end.
---
+++ Paskal Задание №2.pas
@@ -0,0 +1,39 @@
+﻿Program DKR_2;
+var
+  str, ministr, newStr: string;
+  i, j, k, y: integer;
+
+begin
+  writeln('Введите строку: ');
+  readln(str);
+  writeln('Введите подстроку для инверсии: ');
+  readln(ministr);
+
+  i := 1;
+  while i <= length(str) do
+  begin
+    j := 1;
+    k := i;
+    while (j <= length(ministr)) and (k <= length(str)) and (str[k] = ministr[j]) do
+    begin
+      j := j + 1;
+      k := k + 1;
+    end;
+
+    if j > length(ministr) then
+    begin
+      for y := length(ministr) downto 1 do
+      begin
+        newStr := newStr + ministr[y];
+      end;
+      i := i + length(ministr);
+    end
+    else
+    begin
+      newStr := newStr + str[i];
+      i := i + 1;
+    end;
+  end;
+
+  writeln(newStr);
+end.
